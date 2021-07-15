4c0b06ee902b8646dd03f4a416fa805480c6d212
SomTambe <tambesom@gmail.com>
2021-07-15 13:32:17 +0530

    Add correct function s

--- kornia vs julia/mytests.jl
+++ kornia vs julia/mytests.jl
@@ -11,12 +11,15 @@ tgt2 = imresize(tgt_img,ratio=1/8)
 src3 = map(x->Gray(x).val, src2)
 tgt3 = map(x->Gray(x).val,tgt2)
 function warp_me2(img::AbstractArray{T}, tform) where T
-    img = ImageTransformations.box_extrapolation(img, Flat())
-    inds = DiffImages.custom_autorange(img, inv(tform))
-    inds = map(x->SVector{length(x),Float64}(x.I),CartesianIndices(inds))
-    img = map(x -> ImageTransformations._getindex(img, tform(x)), inds)
-    return img
-end;
+   img = ImageTransformations.box_extrapolation(img, RGB(.0,.0,.0))
+   # inds = DiffImages.custom_autorange(img, inv(tform))
+   inds = map(x->SVector{length(x),Float64}(x.I),CartesianIndices(img))
+   # tgts = H * srcs
+   # img = map(x -> ImageTransformations._getindex(img, tform(x)), inds)
+   img = map(x->img(inv(tform)(x)...),inds)
+   return img
+end
+
 
 h=homography2d(srcs,tgts);
 h=DiffImages.homography{Float64}(h);
